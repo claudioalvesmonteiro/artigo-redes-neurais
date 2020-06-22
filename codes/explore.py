ea2d5f2c7399f094db615235a80d29abe4a9b13c
Cláudio Monteiro <clam@cin.ufpe.br>
2020-06-22 09:49:48 -0300

    init

---
+++ codes/explore.py
@@ -0,0 +1,18 @@
+
+
+exec(open('C:/Tools/spark/spark3/python/pyspark/shell.py').read())
+import pyspark.sql.functions as SF
+
+
+# pegar recife 2019
+# pegar recife 2018
+
+# ver estudantes antes do ultimo ano 
+# que estavam em 2018 e n estao em 2019
+# criar var de evasao
+
+
+# levantar variaveis independentes e pre-processamento
+
+# salvar base
+

bf12ad75bdd4ff2dcfffeaab00f92b5dc2bda218
claudiopacha <claudiomonteiro@protonmail.com>
2020-06-25 17:33:52 -0300

    up_spark

--- codes/explore.py
+++ codes/explore.py
@@ -23,42 +23,43 @@ exec(open('/home/pacha/spark/python/pyspark/shell.py').read())
 import pyspark.sql.functions as SF
 
 def importCensoEscolar(lista):
-    '''
+    ''' importar dados de matricula, combinando regioes
     '''
     for i in range(len(lista)):
         print(lista[i])
         if i == 0:
-            df = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/MATRICULA_'+lista[i]+'.CSV', header=True, inferSchema=True, sep='|')
+            matricula = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/MATRICULA_'+lista[i]+'.CSV', header=True, inferSchema=True, sep='|')
         else:
-            df2 = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/MATRICULA_'+lista[i]+'.CSV', header=True, inferSchema=True, sep='|')
-            df = df.union(df2)
-    return df
+            mat_temp = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/MATRICULA_'+lista[i]+'.CSV', header=True, inferSchema=True, sep='|')
+            matricula = matricula.union(mat_temp)
+    return matricula
 
-# capturar dados das regioes
+# capturar dados de matricula
 regioes =  ['NORDESTE',  'SUDESTE', 'NORTE',  'SUL', 'CO']
-censo19 = importCensoEscolar(regioes)
-
-#
-censo19 = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/MATRICULA_NORDESTE.CSV', header=True, inferSchema=True, sep='|')
-
+matricula = importCensoEscolar(regioes)
 
 # selecionar ensino medio
-censo19 = censo19.filter((censo19.TP_ETAPA_ENSINO >= 25) & (censo19.TP_ETAPA_ENSINO <= 27) )
+matricula = matricula.filter((matricula.TP_ETAPA_ENSINO >= 25) & (matricula.TP_ETAPA_ENSINO <= 27) )
 
-# remover casos nulos
-#censo19 = censo19.filter(censo19['TP_ETAPA_ENSINO'].isNotNull() )
+# importar dados de escola e renomear colunas
+escola = spark.read.csv('data/microdados_educacao_basica_2019/DADOS/ESCOLAS.CSV', header=True, inferSchema=True, sep='|')
+escola = escola.select(*(SF.col(x).alias(x + '_esc') for x in escola.columns))
+
+# combinar matricula com escola
+data = matricula.join(escola,matricula.CO_ENTIDADE == escola.CO_ENTIDADE_esc,'inner')
 
 # combinar com idade_serie correta para  criar variavel alvo
 idade_serie = spark.read.csv('artigo-redes-neurais/idade_serie.csv', header=True, inferSchema=True, sep=';')
 
-data = censo19.join(idade_serie,(censo19.TP_ETAPA_ENSINO==idade_serie.TP_ETAPA_ENSINO_id)&(
-                                 censo19.NU_IDADE==idade_serie.NU_IDADE_id),'left')
+data = data.join(idade_serie,(data.TP_ETAPA_ENSINO==idade_serie.TP_ETAPA_ENSINO_id)&(
+                                 data.NU_IDADE==idade_serie.NU_IDADE_id),'left')
 
-data.count()
 # tirar amostra aleatoria
-datax = data.sample(False, 0.01, 45)
+datax = data.sample(False, 0.04, 45)
 
-# transformar em pandas DF e savar
+datax.select('CO_MUNICIPIO', 'CO_MUNICIPIO_esc').show()
+
+# transformar em pandas DF e salvar
 datapd = datax.toPandas()
 datapd.to_csv('data.csv', index = False)
 
